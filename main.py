fc9223c1d80d051e06d0915ca67f2414ec52e195
grldbelen <124864198+grldbelen@users.noreply.github.com>
2023-05-25 13:14:54 +0000

    ints and floats in python

---
+++ main.py
@@ -0,0 +1,11 @@
+#Ints - short for integer, a whole number
+#Floats - number with decimal places
+
+#example of Ints
+day = 21
+temp = -15
+
+#example of Floats
+weight = 72.3127
+
+print(weight * 2)
